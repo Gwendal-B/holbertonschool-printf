b2570282acb81331d80d4b3e1149b51e28df77f5
GigiCorlay <11489@holbertonstudents.com>
2025-11-24 23:02:32 +0100

    Define format_specifier struct in main.h
    Added a struct to associate format specifiers with their handling functions.

--- main.h
+++ main.h
@@ -10,4 +10,19 @@ int _printf(const char *format, ...);
 int print_string(va_list args);
 int print_percent(va_list args);
 
+/**
+ * struct format_specifier - associates a format character with a function
+ *
+ * @spec: the format specifier character, e.g., 'c', 's', '%'
+ * @f: pointer to the function that handles this format
+ */
+typedef struct format_specifier
+{
+	char spec;
+	int (*f)(va_list);
+} format_specifier;
+
+/* Function prototype to convert the format string */
+int convertion(const char *format, format_specifier checker[], va_list parameter);
+
 #endif /*MAIN_H*/
