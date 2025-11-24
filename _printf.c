dde6c7814ceb05c45d89527935d6aa93a308d1af
GigiCorlay <11489@holbertonstudents.com>
2025-11-24 23:13:11 +0100

    Add _printf function for formatted output
    Implement the _printf function to handle various format specifiers.

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: the format string containing characters and format specifiers
+ * 
+ * Return: the number of characters printed, or -1 on error
+ */
+int _printf(const char *format, ...)
+{
+  int count;
+  va_list args;
+  
+  format_specifier checker[] = {
+    {'c', print_char},
+    {'s', print_string},
+    {'%', print_percent},
+    {'i', print_all_base},
+    {'d', print_base_ten},
+    {'\0', NULL} 
+};
+
+  if (format == NULL)
+    return (-1);
+
+  va_start(args, format);
+
+  count = convertion(format, checker, args);
+  
+  va_end(args);
+
+  return (count);
+}
+
+
