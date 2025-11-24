31a94ef8b3659f683f493bc9483adbe85da2ce04
Gwendal-B <12309@holbertonstudents.com>
2025-11-24 15:51:38 +0100

    add : _printf + fix : main

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _printf - reproduit le comportement de printf
+ * @format: chaîne contenant du texte + formats
+ *
+ * Return: nombre de caractères a imprimer
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+				count += print_percent();
+			else
+				return (-1);
+		}
+		else
+			count += _putchar(format[i]);
+			i++;
+	}
+	va_end(args);
+	return (count);
+}
--- main.h
+++ main.h
@@ -5,7 +5,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * @args: arguments list
+ */
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+print_char(va_list args);
+print_string(va_list args);
+print_percent(void);
+print_int(va_list args);
+
 
 #endif /*MAIN_H*/
