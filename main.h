7245b8268c1f9246d5df4b88828baa63e6a9bd21
GigiCorlay <11489@holbertonstudents.com>
2025-11-25 17:34:01 +0100

    Add header file main.h with function declarations

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct spec - correspondance entre un format et une fonction
+ * @spec: le caractère du format (ex : 'c', 's', 'd')
+ * @f: pointeur vers la fonction associée
+ */
+typedef struct spec
+{
+	char spec;
+	int (*f)(va_list);
+} spec_t;
+
+/* args - arguments*/
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_int(va_list args);
+int print_percent(va_list args);
+
+#endif
